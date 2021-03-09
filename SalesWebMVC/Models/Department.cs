0b1fbbf5bf6230ad584c3c99a809e30b4dd00b1e
jeanmichael1993 <jeanmichael760@gmail.com>
2021-03-09 14:46:13 -0300

    Mysql adaption and first migration

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -7,13 +7,7 @@ namespace SalesWebMVC.Models
 {
     public class Department
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-
-        public Department(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }

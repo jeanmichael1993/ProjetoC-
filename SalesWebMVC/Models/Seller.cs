e72fd6a10ce65d5c5afb26b0f3d2f7646385af52
jeanmichael1993 <jeanmichael760@gmail.com>
2021-03-11 15:54:49 -0300

    Validation

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,15 +9,24 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} requerido")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} Tamanho deve estar entre {2} e {1}")]
         public String Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
-        public String  Email { get; set; }
+        [Required(ErrorMessage = "{0} requerido")]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        public String Email { get; set; }
+
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} requerido")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Base Salary")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
 
         public Department Department { get; set; }
@@ -48,7 +57,7 @@ namespace SalesWebMVC.Models
             Sales.Remove(sr);
         }
 
-        public double TotalSales(DateTime inital,DateTime final)
+        public double TotalSales(DateTime inital, DateTime final)
         {
             return Sales.Where(p => p.Date >= inital && p.Date <= final).Sum(p => p.Amount);
         }
